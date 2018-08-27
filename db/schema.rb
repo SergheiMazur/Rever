771222ec0be394d99fcee50bf75d54fcd2c037b8
alexandreoliviernycz <nycz.ao@gmail.com>
2018-08-27 14:32:12 -0400

    trying to go in my branch

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_153046) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "imagelink"
+    t.string "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
