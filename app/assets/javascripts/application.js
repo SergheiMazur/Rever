fcbcfc2db0fc829ef0a5490f4a193cbc2eff385c
Elie Harfouche <ehfche@gmail.com>
2018-08-29 12:19:32 -0400

    newlandingbanner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,20 @@
 //= require rails-ujs
 //= require jquery
 //= require_tree .
+
+$(document).ready(function(){
+  var mouseX, mouseY;
+  var ww = $( window ).width();
+  var wh = $( window ).height();
+  var traX, traY;
+  $(document).mousemove(function(e){
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = ((4 * mouseX) / 570) + 40;
+    traY = ((4 * mouseY) / 570) + 50;
+    console.log(traX);
+    $(".title2").css({"background-position": traX + "%" + traY + "%"});
+  });
+});
+
+
