3fa275d8a32d84365c2e7c8dbb14405e6e05a8f9
SergheMazur <serhio1007@gmail.com>
2018-08-30 11:18:24 -0400

    heroku assets

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module SkeletonRever
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
